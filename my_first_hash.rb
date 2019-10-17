b625b319aef06039822eb0dfaa1ba5ed6e1121eb
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-17 09:02:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,8 +40,8 @@ shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
